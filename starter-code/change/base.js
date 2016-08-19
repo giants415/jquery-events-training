b0e7fd8a5a2b724dd6937ae2c9f0363d23195bbb
Andrew <andrew.vinocur@gmail.com>
2016-08-18 19:10:44 -0700

    1, 2 complete. 3 TBD. 4, 5 are undone

--- starter-code/change/base.js
+++ starter-code/change/base.js
@@ -1,7 +1,55 @@
 console.log("Sanity Check: JS is working!");
 
-$(document).ready(function(){
-
+$(document).ready(function() {
   $("#time").text( Date.now() );
-
+  $("#left").change(function() {
+    var update1 = $(this).val();
+    console.log(update1);
+  })
+  $("#right").change(function() {
+    var update2 = $(this).val();
+    console.log(update2);
+  })
+  // var final = function(update1, update2) {
+  //   var sum = ('#left').val(this) + ('#right').val(this);
+  //   return sum;
+  //   console.log(sum);
+  // }
+  // $("#total").val(sum);
 })
+
+
+//     var inputValue = update + $("#total").val();
+//     $("#total").val(update);
+//     console.log(inputValue);
+//   })
+// })
+///// figure out how to separate the 2 update values and then, figure out how to add those to add those 2 boxes together
+
+
+/* $("total").on("click", function(event){
+  event.
+}
+
+)
+
+
+$(document).ready(function(){
+    var a = $("#left").val();
+    var b = $("#right").val();
+    $("total").on("click", function(){
+        var total = a + b;
+        return total;
+    })
+}) */
+
+
+// This code did not work
+// $(document).ready(function() {
+//   $("#time").text( Date.now() );
+//     var a = $("#left").val();
+//     var b = $("#right").val();
+//     $("total").on("click", function(){
+//         var total = a + b;
+//         return total; } )
+//       } )
--- starter-code/submit/base.js
+++ starter-code/submit/base.js
@@ -3,7 +3,10 @@ console.log("Sanity Check: JS is working!");
 var rick_roll_youtube_embed_code = "<iframe width='420' height='315' src='https://www.youtube.com/embed/dQw4w9WgXcQ' frameborder='0' allowfullscreen></iframe>";
 
 $(document).ready(function(){
-
+  $("form").on("click", function(event){
+    event.preventDefault(); })
   $("#time").text( Date.now() );
+ })
 
-})
+//Still need to do: stretch goal of embedding video on click
+// buttons are "input"
